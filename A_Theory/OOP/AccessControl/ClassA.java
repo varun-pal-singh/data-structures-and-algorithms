ba773f886745f75b18d435bc42368f0fc7f78c62
varun <singhvarun928@gmail.com>
2023-08-21 08:56:43 +0530

    completed Access Control

---
+++ A_Theory/OOP/AccessControl/ClassA.java
@@ -0,0 +1,21 @@
+// package A_Theory.OOP.AccessControl.ClassA;
+
+public class ClassA {
+    private int num;
+    String name;
+    int[] arr;
+
+    public ClassA(int num, String name){
+        this.num = num;
+        this.name = name;
+        this.arr = new int[num];
+    }
+
+    public int getNum(){
+        return num;
+    }
+
+    public void setNum(int num){
+        this.num = num;
+    }
+}
