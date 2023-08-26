b499115b04d07682ffa945f7e90b8bbcc692a3f1
varun <singhvarun928@gmail.com>
2023-08-26 21:54:17 +0530

    some changes

---
+++ A_Theory/OOP/GenericsLambdaException/LambdaFuntions.java
@@ -0,0 +1,16 @@
+package A_Theory.OOP.GenericsLambdaException;
+
+import java.util.ArrayList;
+
+public class LambdaFuntions {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            arr.add(i);
+        }
+        arr.forEach((item) -> System.out.print(item * 2+" "));
+    }
+    int sum(int a, int b){
+        return a+b;
+    }
+}
--- B_Practice/B_Sorting/Selectionsort.java
+++ B_Practice/B_Sorting/Selectionsort.java
@@ -2,6 +2,6 @@ package B_Practice.B_Sorting;
 
 class Selectionsort{
     public static void main(String[] args) {
-       
+        System.out.println("Hello World!!");
     }
 }
