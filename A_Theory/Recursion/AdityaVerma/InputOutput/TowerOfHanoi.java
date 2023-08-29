8a3fd14dc1c479144bad5bd30eb6c5d1cdcd0543
varun <singhvarun928@gmail.com>
2023-08-29 18:41:40 +0530

    does some recursion questions

---
+++ A_Theory/Recursion/AdityaVerma/InputOutput/TowerOfHanoi.java
@@ -0,0 +1,17 @@
+package A_Theory.Recursion.AdityaVerma.InputOutput;
+
+public class TowerOfHanoi {
+    public static void main(String[] args) {
+        solve(3, "source", "destination", "helper");
+    }
+    static void solve(int n, String s, String d, String h){
+        if(n == 1){
+            System.out.println("Moving plate "+n+" from "+s+" to "+d);
+            return;
+        }
+        solve(n - 1, s, h, d);
+        System.out.println("Moving plate "+n+" from "+s+" to "+h);
+        solve(n - 1, h, d, s);
+    }
+}
+ 
