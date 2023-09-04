06e1eda3e43180f3d3481a6c91137ed33f3f123d
varun <singhvarun928@gmail.com>
2023-09-04 05:43:10 +0530

    recursion

---
+++ A_Theory/Recursion/AdityaVerma/SmallerInput/JosephusProblem.java
@@ -0,0 +1,14 @@
+package A_Theory.Recursion.AdityaVerma.SmallerInput;
+
+public class JosephusProblem {
+    public static void main(String[] args) {
+        int n = 40, k = 7;
+        System.out.println(givePosition(n, k));
+    }
+
+    static int givePosition(int n, int k){
+        if(n <= 0){
+            
+        }
+    }
+}
