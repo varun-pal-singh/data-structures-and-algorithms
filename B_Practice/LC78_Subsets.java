eaf20402db48b4b74f0bd01503370ff16b3e7bd4
varun <singhvarun928@gmail.com>
2023-09-07 19:31:03 +0530

    done LC78_subsets by myself

--- B_Practice/LC78_Subsets.java
+++ B_Practice/LC78_Subsets.java
@@ -1,6 +1,12 @@
 package B_Practice;
 import java.util.ArrayList;
 import java.util.List;
+
+/*
+ * given [1, 2, 3]
+ * op : [[], [1], [2], [3], [1,2], [1, 3], [2, 3], [1, 2, 3]];
+ */
+
 public class LC78_Subsets {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3};
@@ -8,18 +14,21 @@ public class LC78_Subsets {
     }
     public static List<List<Integer>> generateSubsets(int[] nums){
         List<List<Integer>> list = new ArrayList<>();
-        return solve(nums, 0, list, new ArrayList<>());
-    }
-    static List<List<Integer>> solve(int[] nums, int idx, List<List<Integer>> list, List<Integer> op){
-        if(idx == nums.length - 1){
-            list.add(op);
-            // System.out.println(list);
-            return list;
-        }
-        list.addAll(solve(nums, idx + 1, list, op));
-        op.add(nums[idx]);
-        list.addAll(solve(nums, idx + 1, list, op));
-        System.out.println(list);
+        List<Integer> op = new ArrayList<>();
+        solve(nums, 0, op, list);
+        System.out.println("op list after process : "+op);
+        System.out.println("size of list of list of integer : "+list.size());
         return list;
+    }
+    private static void solve(int[] nums, int idx, List<Integer> op, List<List<Integer>> list){
+        if(idx >= nums.length){
+            List<Integer> temp = new ArrayList<>(op);
+            list.add(temp);
+            return;
+        }
+        solve(nums, idx+1, op, list);
+        op.add(nums[idx]);
+        solve(nums, idx+1, op, list);
+        op.remove(op.size() - 1);
     }
 }
