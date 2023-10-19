8a196c4705dbc92611267a33fa3619a4d52e5a88
Varun Pal Singh <singhvarun928@gmail.com>
2023-10-19 23:35:43 +0530

    group anagrams

---
+++ src/com/practice/leetCode/top150/medium/LC49_group_anagrams.java
@@ -0,0 +1,34 @@
+package com.practice.leetCode.top150.medium;
+
+/**
+ * Given an array of strings strs, group the anagrams together.
+ * You can return the answer in any order.
+
+ * An Anagram is a word or phrase formed by rearranging the
+ * letters of a different word or phrase, typically using all
+ * the original letters exactly once.
+
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+ * Input: strs = [""]
+ * Output: [[""]]
+
+ * Input: strs = ["a"]
+ * Output: [["a"]]
+
+ * Constraints:
+ * 1 <= strs.length <= 104
+ * 0 <= strs[i].length <= 100
+ * strs[i] consists of lowercase English letters.
+ */
+import java.util.*;
+public class LC49_group_anagrams {
+    public static void main(String[] args) {
+
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        return new ArrayList<>();
+    }
+}
