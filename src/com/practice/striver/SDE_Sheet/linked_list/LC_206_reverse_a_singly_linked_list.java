75e21ca186c8e9ec2150c3665a91205753157d98
Varun Pal Singh <singhvarun928@gmail.com>
2023-10-29 21:56:02 +0530

    linked list

---
+++ src/com/practice/striver/SDE_Sheet/linked_list/LC_206_reverse_a_singly_linked_list.java
@@ -0,0 +1,7 @@
+package com.practice.striver.SDE_Sheet.linked_list;
+import com.dsa.linkedList.customLL.MySinglyLinkedList;
+public class LC_206_reverse_a_singly_linked_list {
+    public static void main(String[] args) {
+
+    }
+}
