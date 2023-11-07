7e6c505ce74d601ac88d53dbd6ba919ff39c24b4
Varun Pal Singh <singhvarun928@gmail.com>
2023-11-07 14:00:16 +0530

    use java.net

---
+++ src/com/theory/socket_programming/practical1.java
@@ -0,0 +1,15 @@
+package com.theory.socket_programming;
+import java.io.*;
+import java.net.*;
+
+public class practical1 {
+    public static void main(String[] args) {
+        try{
+            InetAddress ip = InetAddress.getByName("www.google.com");
+            System.out.println("Host Name: "+ ip.getHostName());
+            System.out.println("IP Address: "+ ip.getHostAddress());
+        }catch(Exception err){
+            System.out.println(err);
+        }
+    }
+}
