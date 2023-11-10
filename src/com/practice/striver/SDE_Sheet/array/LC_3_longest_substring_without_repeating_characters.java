4b936b2b6ba3ba5e6ff3afd40f3a6c0dee58c3c9
Varun Pal Singh <singhvarun928@gmail.com>
2023-11-10 23:05:28 +0530

    longest substring

---
+++ src/com/practice/striver/SDE_Sheet/array/LC_3_longest_substring_without_repeating_characters.java
@@ -0,0 +1,31 @@
+package com.practice.striver.SDE_Sheet.array;
+/**
+ * Given a string s, find the length of the longest substring
+ * without repeating characters.
+
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+ * Constraints:
+ * 0 <= s.length <= 5 * 10^4
+ * s consists of English letters, digits, symbols and spaces.
+ */
+public class LC_3_longest_substring_without_repeating_characters {
+    public static void main(String[] args) {
+
+    }
+    public int lengthOfLongestSubstring(String s) {
+
+        return -1;
+    }
+}
