2e05e313f5ffee6b75885c7d2cc750245dcf0d65
Varun Pal Singh <singhvarun928@gmail.com>
2023-11-12 09:09:56 +0530

    some more questions

--- src/com/practice/striver/SDE_Sheet/array/LC_18_4sum.java
+++ src/com/practice/striver/SDE_Sheet/array/LC_18_4sum.java
@@ -22,13 +22,37 @@ package com.practice.striver.SDE_Sheet.array;
  */
 import java.util.List;
 import java.util.ArrayList;
-
+import java.util.Arrays;
 public class LC_18_4sum {
     public static void main(String[] args) {
-
+        int[] nums1 = {1,0,-1,0,-2,2}, nums2 = {2,2,2,2,2};
+        int target1 = 0, target2 = 8;
+        System.out.println("output for nums1 : "+ fourSum(nums1, target1));
+        System.out.println("output for nums1 : "+ fourSum(nums2, target2));
     }
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-
-        return new ArrayList<>();
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;   // avoiding duplicates
+            for(int j = i + 1; j < n; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;   // avoiding duplicates
+                int k = j + 1, l = n - 1;
+                while(k < l){
+                    long sum = nums[i] + nums[j];
+                    sum += nums[k] + nums[l];
+                    if(sum < (long) target)     k++;
+                    else if(sum > (long) target)    l--;
+                    else{
+                        list.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++; l--;
+                        while(k < l && nums[k] == nums[k - 1])  k++;
+                        while(k < l && nums[l] == nums[l + 1])  l--;
+                    }
+                }
+            }
+        }
+        return list;
     }
 }
--- src/com/practice/striver/SDE_Sheet/array/Largest_subarray_with_sum_zero.java
+++ src/com/practice/striver/SDE_Sheet/array/Largest_subarray_with_sum_zero.java
@@ -6,20 +6,46 @@ package com.practice.striver.SDE_Sheet.array;
  */
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+
 public class Largest_subarray_with_sum_zero {
     public static void main(String[] args) {
         ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1, 3, -1, 4, -4));
         ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1, -1, 2, -2));
-        System.out.println("length of largest subarray with sum 0 is "+LongestSubsetWithZeroSum(list1));
-        System.out.println("length of largest subarray with sum 0 is "+LongestSubsetWithZeroSum(list2));
+        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(1, 3, 2, 1, 1, 1, 2, 6));
+        System.out.println("length of largest subarray with sum 0 is "+LongestSubsetWithZeroSumValidForNeqZerosPostives(list1));
+        System.out.println("length of largest subarray with sum 0 is "+LongestSubsetWithZeroSumValidForNeqZerosPostives(list2));
+        System.out.println("length of largest subarray with sum 0 if arr contains only +ve : "+ longestSubsetWithKSumContainsOnlyPostives(list3, 5));
     }
-    public static int LongestSubsetWithZeroSum(ArrayList<Integer> arr) {
+
+    public static int LongestSubsetWithZeroSumValidForNeqZerosPostives(ArrayList<Integer> arr) {
+        // arr contains -ve, zero, and +ve than this is optimal
+        HashMap<Integer, Integer> map = new HashMap<>();
         int n = arr.size(), curr_size = 0, max_size = 0, prefixSum = 0;
         for(int i = 0; i < n; i++){
             prefixSum += arr.get(i);
             if(prefixSum == 0)  curr_size = i;
-
+//            int rem = prefixSum;
+            curr_size += map.getOrDefault(prefixSum, 0);
+            map.put(prefixSum, i);
+            max_size = Math.max(max_size, curr_size);
         }
-        return prefixSum;
+        return max_size;
+    }
+    public static int longestSubsetWithKSumContainsOnlyPostives(ArrayList<Integer> list, int k){
+        // [1, 3, 2, 1, 1, 1, 2, 6]
+        int n = list.size(), prefixSum = 0;
+        int left = 0, right = 0, maxLen = 0;
+        while(right < n){
+            prefixSum += list.get(right);
+            while(left <= right && prefixSum > k){
+                prefixSum -= list.get(left++);
+            }
+            if(prefixSum == k){
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            right++;
+        }
+        return maxLen;
     }
 }
