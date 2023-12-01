d26e7271debfd7f4fc0a0a88c7ad3ea00c1311cd
Varun Pal Singh <singhvarun928@gmail.com>
2023-12-01 22:50:27 +0530

    added some question

---
+++ src/com/practice/striver/SDE_Sheet/array/LC_42_trapping_rain_water.java
@@ -0,0 +1,29 @@
+package com.practice.striver.SDE_Sheet.array;
+/**
+ * Given n non-negative integers representing an elevation map
+ * where the width of each bar is 1, compute how much water it
+ * can trap after raining.
+
+ * Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * Output: 6
+ * Explanation: The above elevation map (black section) is
+ * represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case,
+ * 6 units of rain water (blue section) are being trapped.
+
+ * Input: height = [4,2,0,3,2,5]
+ * Output: 9
+
+ * Constraints:
+ * n == height.length
+ * 1 <= n <= 2 * 104
+ * 0 <= height[i] <= 105
+ */
+public class LC_42_trapping_rain_water {
+    public static void main(String[] args) {
+
+    }
+    public static int trap(int[] height) {
+
+        return -1;
+    }
+}
