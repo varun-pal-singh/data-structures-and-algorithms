aebb2fc59664f5efa2d5be4645166e4fa5e1fd4f
Varun Pal Singh <singhvarun928@gmail.com>
2023-12-06 23:55:36 +0530

    added questions

---
+++ src/com/practice/striver/SDE_Sheet/greedy/Job_sequencing_problem.java
@@ -0,0 +1,31 @@
+package com.practice.striver.SDE_Sheet.greedy;
+/**
+ * Problem Statement: You are given a set of N jobs where each
+ * job comes with a deadline and profit. The profit can only be
+ * earned upon completing the job within its deadline. Find the
+ * number of jobs done and the maximum profit that can be
+ * obtained. Each job takes a single unit of time and only one
+ * job can be performed at a time.
+
+ * Input: N = 4, Jobs = {(1,4,20),(2,1,10),(3,1,40),(4,1,30)}
+
+ * Output: 2 60
+
+ * Explanation: The 3rd job with a deadline 1 is performed
+ * during the first unit of time .The 1st job is performed
+ * during the second unit of time as its deadline is 4.
+ * Profit = 40 + 20 = 60
+
+ * Input: N = 5, Jobs = {(1,2,100),(2,1,19),(3,2,27),(4,1,25),(5,1,15)}
+
+ * Output: 2 127
+
+ * Explanation: The  first and third job both having a deadline
+ * 2 give the highest profit.
+ * Profit = 100 + 27 = 127
+ */
+public class Job_sequencing_problem {
+    public static void main(String[] args){
+
+    }
+}
