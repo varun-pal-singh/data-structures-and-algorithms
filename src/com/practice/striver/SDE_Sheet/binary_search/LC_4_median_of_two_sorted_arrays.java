172c7e88b3e5b93fd1e7cc5a7a2dd6fd51e1171c
Varun Pal Singh <singhvarun928@gmail.com>
2023-12-09 23:41:36 +0530

    median of two sorted arrays

---
+++ src/com/practice/striver/SDE_Sheet/binary_search/LC_4_median_of_two_sorted_arrays.java
@@ -0,0 +1,32 @@
+package com.practice.striver.SDE_Sheet.binary_search;
+/**
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+ * The overall run time complexity should be O(log (m+n)).
+
+ * Input: nums1 = [1,3], nums2 = [2]
+ * Output: 2.00000
+ * Explanation: merged array = [1,2,3] and median is 2.
+
+ * Input: nums1 = [1,2], nums2 = [3,4]
+ * Output: 2.50000
+ * Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+ * Constraints:
+
+ * nums1.length == m
+ * nums2.length == n
+ * 0 <= m <= 1000
+ * 0 <= n <= 1000
+ * 1 <= m + n <= 2000
+ * -10^6 <= nums1[i], nums2[i] <= 10^6
+ */
+public class LC_4_median_of_two_sorted_arrays {
+    public static void main(String[] args) {
+
+    }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        return -1;
+    }
+}
