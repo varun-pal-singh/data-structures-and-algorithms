a44da8c0dfa1765a6547958ff96a1891017102e6
Varun Pal Singh <singhvarun928@gmail.com>
2023-12-11 23:26:21 +0530

    fractional knapsack

---
+++ src/com/practice/striver/SDE_Sheet/greedy/Pair.java
@@ -0,0 +1,11 @@
+package com.practice.striver.SDE_Sheet.greedy;
+
+public class Pair {
+    int weight;
+    int value;
+    Pair(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+
+}
